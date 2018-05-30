c89767d04d3744b6026aabb97fc41e4872154714
tiagovit <tiago.vit@hotmail.com>
2018-05-30 15:11:59 +0100

    sd

--- chat/js/chat.js
+++ chat/js/chat.js
@@ -7,13 +7,26 @@ $(document).ready(function () {
 function init() {
 
 
-    $('button').click(function(){
+    $('.logout').click(function(){
 
         firebase.auth().signOut().then(function() {
             window.location.href = "login.html";
         })
 
     });
+
+    $('.update').click(function () {
+
+        var user = firebase.auth().currentUser;
+
+        user.updateProfile({
+            displayName: "tiagovit"
+        }).then(function() {
+            console.log(firebase.auth().currentUser);
+        }).catch(function(error) {
+            // An error happened.
+        });
+    })
 
 }
 
