bd219dc4d0fc1d118c381454bb12630a2bfcaddb
tiagovit <tiago.vit@hotmail.com>
2018-07-17 17:23:06 +0100

    ads

--- angularJs/angularjavascript.js
+++ angularJs/angularjavascript.js
@@ -1,8 +1,10 @@
-angular.module('MyApp',['ngMaterial'])
+angular.module('MyApp',['ngMaterial','duScroll'])
     .run(function () {
         console.log('MyApp is ready!');
 
-    }).controller("Ctrl1", function ($scope, $timeout, $mdBottomSheet, $mdToast) {
+    }).controller("Ctrl1", function ($scope, $timeout, $mdBottomSheet, $mdDialog, $document) {
+
+        getDatabaseItems();
 
 
     $.getJSON("databases/dbCountryCodes.json", function(json) {
@@ -24,16 +26,21 @@ angular.module('MyApp',['ngMaterial'])
         $scope.dbCurrency = arr;
     });
 
-    db.collection('country').get().then((snapshot) => {
-        var dataToTargetFunction = [];
-        snapshot.docs.forEach(doc => {
-            dataToTargetFunction.push(doc.data());
-        });
-        console.log(dataToTargetFunction);
-        $scope.dbCountryFromFirestore = dataToTargetFunction;
-        $scope.$applyAsync();
 
-    });
+    function getDatabaseItems(){
+
+        db.collection('country').get().then((snapshot) => {
+            var dataToTargetFunction = [];
+            snapshot.docs.forEach(doc => {
+                dataToTargetFunction.push(doc.data());
+            });
+            console.log(dataToTargetFunction);
+            $scope.dbCountryFromFirestore = dataToTargetFunction;
+            $scope.$digest();
+
+        });
+    }
+
 
 
 
@@ -52,5 +59,86 @@ angular.module('MyApp',['ngMaterial'])
         }).catch(function(error) {
         });
     };
+
+    $scope.submitCountryForm = function () {
+        countryForm = $('#newCountryForm');
+
+
+        var mCountry = countryForm[0].country.value;
+        var mCapital = countryForm[0].capital.value;
+        var mGdp = countryForm[0].gdp.value;
+        var mPopulation = countryForm[0].population.value;
+        var mIsoCode = $scope.countryForm.isoCode;
+        var mCurrency = $scope.countryForm.currency;
+        //console.log(mCountry,mCapital,mGdp,mPopulation,mIsoCode, mCurrency);
+
+        newCountryObject = new Object({
+            capital : mCapital,
+            country: mCountry,
+            gdp : mGdp,
+            isoCode : mIsoCode,
+            language : [],
+            money : mCurrency,
+            population : mPopulation
+        });
+
+
+        db.collection('country').doc(mCountry).set({
+            capital : mCapital,
+            country: mCountry,
+            gdp : mGdp,
+            isoCode : mIsoCode,
+            language : [],
+            money : mCurrency,
+            population : mPopulation
+        }).then(function () {
+            $scope.dbCountryFromFirestore.push(newCountryObject);
+            $scope.$digest();
+            $mdBottomSheet.hide();
+            //location.reload();
+        }).catch(function () {
+            alert("There was an error submiting the data");
+        });
+
+
+
+    };
+
+    $scope.showConfirmDialog = function(ev, id) {
+        // Appending dialog to document.body to cover sidenav in docs app
+        console.log(id);
+        var confirm = $mdDialog.confirm({
+            onComplete : function afterShowAnimation() {
+                var $dialog = angular.element(document.querySelector('md-dialog'));
+                var $actionsSection = $dialog.find('md-dialog-actions');
+                var $confirmButton = $actionsSection.children()[1];
+                angular.element($confirmButton).addClass('md-raised md-warn');
+            }
+        })
+            .title('You\'re about to delete '+id)
+            .textContent('All of the data will be lost')
+            .ariaLabel('Country delete')
+            .targetEvent(ev)
+            .ok('delete')
+            .cancel('cancel');
+
+        $mdDialog.show(confirm).then(function() {
+
+            db.collection('country').doc(id).delete().then(function () {
+                document.getElementById(id).remove();
+            }).catch(function () {
+                alert('Unable to remove country')
+            });
+        }, function() {
+            //Something
+        });
+    };
+
+    $scope.scrollToSpain = function () {
+        alert("to asdf");
+        $document.scrollTopAnimated(0, 5000).then(function() {
+            console && console.log('You just scrolled to the top!');
+        });
+    }
 
 });
