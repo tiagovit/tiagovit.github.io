405f5ab02d0e598749e280199b856edaa72fd846
Tiago Vitorino <tiago.vit@hotmail.com>
2018-07-17 19:44:04 +0100

    a

--- angularJs/angularjavascript.js
+++ angularJs/angularjavascript.js
@@ -142,3 +142,22 @@ angular.module('MyApp',['ngMaterial','duScroll'])
     }
 
 });
+
+$( document ).ready(function() {
+
+    var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
+    $('#leftSideDiv').bind(mousewheelevt, function(e){
+
+        var evt = window.event || e //equalize event object
+        evt = evt.originalEvent ? evt.originalEvent : evt; //convert to originalEvent if possible
+        var delta = evt.detail ? evt.detail*(-40) : evt.wheelDelta //check for detail first, because it is used by Opera and FF
+
+        if(delta > 0) {
+            $('#autoCompleteSearch').fadeIn();
+        }
+        else{
+            $('#autoCompleteSearch').fadeOut();
+        }
+    });
+
+});
